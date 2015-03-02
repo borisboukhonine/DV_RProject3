b0ebbaef9d42063e1211e736b8eb461a9e7a4862
Boris Boukhonine <boukhonine@wireless-10-145-177-148.public.utexas.edu>
2015-03-01 21:04:38 -0600

    work in progress3

--- 01 Data/dataframes.R
+++ 01 Data/dataframes.R
@@ -10,14 +10,24 @@ cig_tax %>% inner_join(smoke_free, by="STATES")
 
 View(cig_tax %>% inner_join(smoke_free, by=c("YEAR", "STATES")))
 
+# first vis
 injoined <- cig_tax %>% inner_join(smoke_free, by=c("YEAR", "STATES"))
 injoined %>% mutate(tax_percent=cume_dist(CIGARETTE_TAX_DOLLAR_PER_PACK))
 mut<-injoined %>% mutate(tax_percent=cume_dist(CIGARETTE_TAX_DOLLAR_PER_PACK))
 g <- mut %>% ggplot(aes(x=YEAR, y=tax_percent,color=STATES))+geom_point()+geom_line()
 g+ggtitle('Cigarette Tax % by State per Year')+guides(col=guide_legend(ncol=3))+labs(x='Year', y='Percent Tax on Cigarettes')+theme(axis.title.x=element_text(vjust=-0.35), axis.title.y=element_text(vjust=1.25), plot.title=element_text(vjust=1))
 
+# second vis
+injoined <- cig_tax %>% left_join(smoke_free, by=c("YEAR", "STATES"))
 df <- injoined %>% select(STATES, YEAR, CIGARETTE_TAX_DOLLAR_PER_PACK, TYPE_OF_RESTRICTION) %>% filter(TYPE_OF_RESTRICTION != "No law, designated areas, or separate ventilation law")
 d<- df %>% group_by(STATES) %>% summarize(minyear=min(YEAR), avg_tax=mean(CIGARETTE_TAX_DOLLAR_PER_PACK))
 g<-d %>% ggplot(aes(x = STATES, y = minyear))+geom_point(aes(size=avg_tax, color=avg_tax))+scale_size_continuous(range=c(2,15),name="Average Tax in $\n from 1995-2014")
 g+ggtitle('First Year of Smoking Ban by State,\n with Average Tax in Dollars')+theme(axis.text.x=element_text(angle=50, size=14,vjust=0.5), axis.text.y=element_text(size=14,vjust=0.5), plot.title=element_text(vjust=1, size=25))+labs(x='State', y='First Year of Smoking Ban')+theme(legend.title=element_text(size=17.235235235))+scale_color_continuous(name="Average Tax in $\n from 1995-2014")
 
+# third vis -- we thought utah was doing something weird
+injoined <- cig_tax %>% inner_join(smoke_free, by=c("YEAR", "STATES"))
+
+df <- injoined %>% select(STATES, YEAR, CIGARETTE_TAX_DOLLAR_PER_PACK, TYPE_OF_RESTRICTION) %>% filter(TYPE_OF_RESTRICTION == "No law, designated areas, or separate ventilation law")
+df %>% filter(YEAR %in% c(1995, 2005, 2010)) %>% ggplot(aes(x=STATES, y=CIGARETTE_TAX_DOLLAR_PER_PACK))+geom_point()+facet_wrap(~YEAR)
+g<-df %>% filter(YEAR %in% c(1995, 2005, 2010)) %>% ggplot(aes(x=STATES, y=CIGARETTE_TAX_DOLLAR_PER_PACK, fill=STATES))+geom_bar(stat="identity", position=position_dodge())+guides(col=guide_legend(ncol=3))+facet_wrap(~YEAR, nrow=3)
+g+ggtitle('Cigarette Tax in States Without Smoking Ban:\n1995, 2005, 2010')+guides(col=guide_legend(ncol=3))+theme(axis.title.x=element_text(vjust=-0.35), axis.title.y=element_text(vjust=1.25), plot.title=element_text(vjust=1))+theme(axis.text.x=element_text(angle=50, size=14,vjust=0.5), axis.text.y=element_text(size=14,vjust=0.5), plot.title=element_text(vjust=1, size=25))+labs(x='State', y='First Year of Smoking Ban')+theme(legend.title=element_text(size=17.235235235))
